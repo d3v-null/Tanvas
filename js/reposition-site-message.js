edf6f8693a9f1fd936a9bd176beef4bb02bacccc
derwentx <derwentx@gmail.com>
2016-03-01 18:33:37 +0800

    reposition storewide message

--- js/reposition-site-message.js
+++ js/reposition-site-message.js
@@ -3,7 +3,9 @@
 {
 	$(document).ready(function(){
 		var banner = $("p.demo_store");
-		var wrapper = $("div#wrapper div#inner-wrapper header#header");
+		var wrapper = $("div.off-canvas-wrap");
+        console.log(banner);
+        console.log(wrapper);
 		if(banner.length > 0 && wrapper.length > 0){
 			banner.insertBefore(wrapper);
 		}
